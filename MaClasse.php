ae03b4677f816b2505559a118539a1d79ca9e3e1
Tcharlyson <tcharlyson.platel@ynov.com>
2016-01-12 17:07:40 +0100

    méthode __callStatic pour méthode static inexistante ou privée

--- MaClasse.php
+++ MaClasse.php
@@ -12,8 +12,13 @@ class MaClasse
     {
         echo 'La méthode <strong>', $nom, '</strong> a été appelée alors qu\'elle n\'existe pas ! Ses arguments étaient les suivants : <strong>', implode($arguments, '</strong>, <strong>'), '</strong>';
     }
+    public static function __callStatic($nom, $arguments)
+    {
+        echo 'La méthode <strong>', $nom, '</strong> a été appelée dans un contexte statique alors qu\'elle n\'existe pas ! Ses arguments étaient les suivants : <strong>', implode ($arguments, '</strong>, <strong>'), '</strong><br />';
+    }
 }
 
 $obj = new MaClasse;
 
 $obj->methode(123, 'test');
+MaClasse::methodeStatique(456, 'autre test');

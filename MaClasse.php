26115fc5b0f4a6286008665b8503d3e19fd0c26a
Tcharlyson <tcharlyson.platel@ynov.com>
2016-01-19 18:44:22 +0100

    Throw MyException

--- MaClasse.php
+++ MaClasse.php
@@ -6,11 +6,24 @@
  * Date: 12/01/2016
  * Time: 16:55
  */
+class MonException extends Exception
+{
+    public function __construct($message, $code = 0)
+    {
+        parent::__construct($message, $code);
+    }
+
+    public function __toString()
+    {
+        return $this->message;
+    }
+}
+
 function additionner($a, $b)
 {
     if (!is_numeric($a) || !is_numeric($b))
     {
-        throw new Exception('Les deux paramètres doivent être des nombres'); // On lance une nouvelle exception si l'un des deux paramètres n'est pas un nombre.
+        throw new MonException('Les deux paramètres doivent être des nombres'); // On lance une nouvelle exception si l'un des deux paramètres n'est pas un nombre.
     }
 
     return $a + $b;
@@ -23,7 +36,7 @@ try // On va essayer d'effectuer les instructions situées dans ce bloc.
     echo additionner(4, 8);
 }
 
-catch (Exception $e) // On va attraper les exceptions "Exception" s'il y en a une qui est levée.
+catch (MonException $e) // On va attraper les exceptions "Exception" s'il y en a une qui est levée.
 {
-    echo 'Exception lancé', $e->getMessage();
+    echo $e;
 }

2472ec1e5fd1880516976f0c55e64f909b2f5b5c
Tcharlyson <tcharlyson.platel@ynov.com>
2016-01-19 18:24:17 +0100

    Interface with Iterator

--- MaClasse.php
+++ MaClasse.php
@@ -6,14 +6,55 @@
  * Date: 12/01/2016
  * Time: 16:55
  */
-interface Movable
+class MaClasse implements Iterator
 {
-    const MA_CONSTANTE = 'Hello';
-}
-echo Movable::MA_CONSTANTE;
+    private $position=0;
+    private $tableau=['Premier','Deuxième','Troisième','Quatrième'];
 
-class Personnage implements Movable
+    public function current()
+    {
+        return $this->tableau[$this->position];
+    }
+    public function key()
+    {
+        return $this->position;
+    }
+    public function next()
+    {
+        return $this->position++;
+    }
+    public function rewind()
+    {
+        return $this->position=0;
+    }
+    public function seek($position)
+    {
+        $anciennePosition = $this->position;
+        $this->position=$position;
+        if(!$this->valid())
+        {
+            trigger_error('La position spécifiée nest pas valide', E_USER_WARNING);
+            $this->position=$anciennePosition;
+        }
+    }
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
+
+    public function valid()
+    {
+        return isset($this->tableau[$this->position]);
+    }
+}
+
+$objet = new MaClasse();
+
+foreach($objet as $key => $value)
 {
+    echo $key, '=>', $value, '<br />';
 }
 
-echo Personnage::MA_CONSTANTE;
+$objet->seek(2);
+$objet->rewind();
+echo $objet->current();

6991b21ec443db5c6c6b0a4a3fafe03ba4540ffe
Tcharlyson <tcharlyson.platel@ynov.com>
2016-01-19 18:46:31 +0100

    Throw Exception Multiple Catch

--- MaClasse.php
+++ MaClasse.php
@@ -1,11 +1,4 @@
 <?php
-
-/**
- * Created by PhpStorm.
- * User: tcharlysonplatel
- * Date: 12/01/2016
- * Time: 16:55
- */
 class MonException extends Exception
 {
     public function __construct($message, $code = 0)
@@ -23,20 +16,31 @@ function additionner($a, $b)
 {
     if (!is_numeric($a) || !is_numeric($b))
     {
-        throw new MonException('Les deux paramètres doivent être des nombres'); // On lance une nouvelle exception si l'un des deux paramètres n'est pas un nombre.
+        throw new MonException('Les deux paramètres doivent être des nombres'); // On lance une exception "MonException".
+    }
+
+    if (func_num_args() > 2)
+    {
+        throw new Exception('Pas plus de deux arguments ne doivent être passés à la fonction'); // Cette fois-ci, on lance une exception "Exception".
     }
 
     return $a + $b;
 }
 
-try // On va essayer d'effectuer les instructions situées dans ce bloc.
+try // Nous allons essayer d'effectuer les instructions situées dans ce bloc.
 {
     echo additionner(12, 3), '<br />';
-    echo additionner('azerty', 54), '<br />';
-    echo additionner(4, 8);
+    echo additionner(15, 54, 45), '<br />';
 }
 
-catch (MonException $e) // On va attraper les exceptions "Exception" s'il y en a une qui est levée.
+catch (MonException $e) // Nous allons attraper les exceptions "MonException" s'il y en a une qui est levée.
 {
-    echo $e;
+    echo '[MonException] : ', $e; // On affiche le message d'erreur grâce à la méthode __toString que l'on a écrite.
 }
+
+catch (Exception $e) // Si l'exception n'est toujours pas attrapée, alors nous allons essayer d'attraper l'exception "Exception".
+{
+    echo '[Exception] : ', $e->getMessage(); // La méthode __toString() nous affiche trop d'informations, nous voulons juste le message d'erreur.
+}
+
+echo '<br />Fin du script'; // Ce message s'affiche, cela prouve bien que le script est exécuté jusqu'au bout.

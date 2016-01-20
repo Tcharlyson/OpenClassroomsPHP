170c8f29482c99aff9e726b18347143b25058a99
Tcharlyson <tcharlyson.platel@ynov.com>
2016-01-20 12:04:10 +0100

    Trait

--- MaClasse.php
+++ MaClasse.php
@@ -1,46 +1,24 @@
 <?php
-class MonException extends Exception
+trait MonTrait
 {
-    public function __construct($message, $code = 0)
+    public function hello()
     {
-        parent::__construct($message, $code);
-    }
-
-    public function __toString()
-    {
-        return $this->message;
+        echo 'Hello world !';
     }
 }
 
-function additionner($a, $b)
+class A
 {
-    if (!is_numeric($a) || !is_numeric($b))
-    {
-        throw new MonException('Les deux paramètres doivent être des nombres'); // On lance une exception "MonException".
-    }
-
-    if (func_num_args() > 2)
-    {
-        throw new Exception('Pas plus de deux arguments ne doivent être passés à la fonction'); // Cette fois-ci, on lance une exception "Exception".
-    }
-
-    return $a + $b;
+    use MonTrait;
 }
 
-try // Nous allons essayer d'effectuer les instructions situées dans ce bloc.
+class B
 {
-    echo additionner(12, 3), '<br />';
-    echo additionner(15, 54, 45), '<br />';
+    use MonTrait;
 }
 
-catch (MonException $e) // Nous allons attraper les exceptions "MonException" s'il y en a une qui est levée.
-{
-    echo '[MonException] : ', $e; // On affiche le message d'erreur grâce à la méthode __toString que l'on a écrite.
-}
+$a = new A;
+$a->hello(); // Affiche « Hello world ! ».
 
-catch (Exception $e) // Si l'exception n'est toujours pas attrapée, alors nous allons essayer d'attraper l'exception "Exception".
-{
-    echo '[Exception] : ', $e->getMessage(); // La méthode __toString() nous affiche trop d'informations, nous voulons juste le message d'erreur.
-}
-
-echo '<br />Fin du script'; // Ce message s'affiche, cela prouve bien que le script est exécuté jusqu'au bout.
+$b = new b;
+$b->hello(); // Affiche aussi « Hello world ! ».in du script'; // Ce message s'affiche, cela prouve bien que le script est exécuté jusqu'au bout.
